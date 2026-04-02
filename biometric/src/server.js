42e4f7b3a9bd040008f574e463e8e82041c7a868
buragaravi <ravi@pydahsoft.in>
2026-04-02 12:10:22 +0530

    testing the manually

--- biometric/src/server.js
+++ biometric/src/server.js
@@ -121,7 +121,7 @@ app.all('*', async (req, res) => {
     }
 
     // Respond with a generic OK explicitly as text/plain (standard for ADMS devices)
-    res.type('text/plain').send('OK');
+    res.status(200).type('text/plain').set('Cache-Control', 'no-store, no-cache').send('OK');
 });
 
 // Connect to MongoDB
